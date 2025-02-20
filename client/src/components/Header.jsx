281b559acce377a9eb582b303ead65a5bf635318
Robertlean <robert_lean@live.com.ar>
2025-02-20 12:29:35 -0300

    created files

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <header>Soy un header</header>
+  )
+}
---
+++ client/src/router/AppRouter.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import HomePage from '../pages/HomePage.jsx'
+import Register from '../pages/Register.jsx'
+import Login from '../pages/Login.jsx'
+
+import Header from '../components/Header.jsx'
+
+export const AppRouter = () => {
+  return (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      
+      <Route exact path="/" element={<HomePage />} /> 
+      <Route exact path="register" element={<Register />} />
+      <Route exact path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+  
+  )
+}
